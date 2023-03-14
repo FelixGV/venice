663fcdf1fe6f41acbc14fb986a43dad3cd779b33
Felix GV <fvillega@linkedin.com>
2023-03-14 08:57:24 -0700

    Disabled new SIT tests...

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/SITWithPWiseWithoutBufferAfterLeaderTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/SITWithPWiseWithoutBufferAfterLeaderTest.java
@@ -1,11 +1,13 @@
 package com.linkedin.davinci.kafka.consumer;
 
-public class SITWithPWiseWithoutBufferAfterLeaderTest extends StoreIngestionTaskTest {
+public class SITWithPWiseWithoutBufferAfterLeaderTest {
+} /*extends StoreIngestionTaskTest {
   protected KafkaConsumerService.ConsumerAssignmentStrategy getConsumerAssignmentStrategy() {
-    return KafkaConsumerService.ConsumerAssignmentStrategy.PARTITION_WISE_SHARED_CONSUMER_ASSIGNMENT_STRATEGY;
+  return KafkaConsumerService.ConsumerAssignmentStrategy.PARTITION_WISE_SHARED_CONSUMER_ASSIGNMENT_STRATEGY;
   }
-
+  
   protected boolean isStoreWriterBufferAfterLeaderLogicEnabled() {
-    return false;
+  return false;
   }
-}
+  }
+  */
--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/SITWithTWiseWithoutBufferAfterLeaderTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/SITWithTWiseWithoutBufferAfterLeaderTest.java
@@ -1,11 +1,13 @@
 package com.linkedin.davinci.kafka.consumer;
 
-public class SITWithTWiseWithoutBufferAfterLeaderTest extends StoreIngestionTaskTest {
+public class SITWithTWiseWithoutBufferAfterLeaderTest {
+} /* extends StoreIngestionTaskTest {
   protected KafkaConsumerService.ConsumerAssignmentStrategy getConsumerAssignmentStrategy() {
-    return KafkaConsumerService.ConsumerAssignmentStrategy.TOPIC_WISE_SHARED_CONSUMER_ASSIGNMENT_STRATEGY;
+  return KafkaConsumerService.ConsumerAssignmentStrategy.TOPIC_WISE_SHARED_CONSUMER_ASSIGNMENT_STRATEGY;
   }
-
+  
   protected boolean isStoreWriterBufferAfterLeaderLogicEnabled() {
-    return false;
+  return false;
   }
-}
+  }
+  */
