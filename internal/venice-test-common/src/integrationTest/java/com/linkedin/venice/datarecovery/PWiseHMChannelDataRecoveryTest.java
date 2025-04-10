792a94f825a9f2b40f048c1bdd2092294240da94
Felix GV <fgv@linkedin.com>
2025-04-10 13:43:29 -0700

    [test] Moved the DataRecoveryTest to its own bucket
    Also added permutations for all shared consumer modes, as well as using
    the (legacy) Helix Message Channel vs Participant Store.
    
    Bumped up the invocation count to 10 for testHybridAADataRecovery, which
    seems to have a bug in it...

---
+++ internal/venice-test-common/src/integrationTest/java/com/linkedin/venice/datarecovery/PWiseHMChannelDataRecoveryTest.java
@@ -0,0 +1,19 @@
+package com.linkedin.venice.datarecovery;
+
+import static com.linkedin.davinci.kafka.consumer.KafkaConsumerService.ConsumerAssignmentStrategy;
+
+import org.testng.annotations.Test;
+
+
+@Test
+public class PWiseHMChannelDataRecoveryTest extends DataRecoveryTest {
+  @Override
+  protected ConsumerAssignmentStrategy getConsumerAssignmentStrategy() {
+    return ConsumerAssignmentStrategy.PARTITION_WISE_SHARED_CONSUMER_ASSIGNMENT_STRATEGY;
+  }
+
+  @Override
+  protected boolean useParticipantStore() {
+    return false;
+  }
+}
